dcc961045f2c6ec00b65d36bc99da592771d75f9
Micah Thomas <61614655+micah-dev@users.noreply.github.com>
2021-10-31 15:03:01 -0400

    park mthd

--- commands/park.js
+++ commands/park.js
@@ -1,22 +1,25 @@
 /**
  * Libraries/Imports
  */
-const {delay} = require("../util");
+const { delay } = require("../util");
 const puppeteer = require('puppeteer');
 const Discord = require('discord.js');
 /**
-  * Local Variables
-  */
-const tempFile = `weather.png`
+ * Local Variables
+ */
+const tempFile = `park.png`
 const tempPath = `./commands/temp/${tempFile}`
-const lookupURL = 'https://www.google.com/search?q=UNCC+weather'
+const lookupURL = 'https://pats.charlotte.edu'
 const logo = 'https://media.discordapp.net/attachments/886347148386529291/894761080348368966/bot_logo-white_on_transparent-06.png?width=850&height=858'
 
+ // Selector Path
+ // body > root > app-root
+
 /**
-  * Command Parameters/Initialization 
-  */
+ * Command Parameters/Initialization 
+ */
 module.exports = {
-    name: 'weather',
+    name: 'park',
     usage: [],
     description: '',
     /**
@@ -33,28 +36,38 @@ module.exports = {
          *   copy>
          *   Selector Path>
          */
-        const browser = await puppeteer.launch()
+        const browser = await puppeteer.launch({headless: false})
         const page = await browser.newPage()
-        await page.goto(lookupURL)
-        const element = await page.$('#rso > div:nth-child(2)'); 
-        delay(500);
-        await element.screenshot({path: tempPath})
+
+        // stolen from interwebs, didnt work
+        await page.goto(lookupURL);
+
+        //await delay(5000)
+        //await page.waitForSelector('#body > root > app-root', { visible: true}); 
+        await delay(500);
+        //const element = await page.$('#two-col-left');
+        //await element.screenshot({path: tempPath})
+
+        // hmnn
+        await page.screenshot({path: tempPath});
+
         /**
          * Using the discord library to make embedded message to send back
          */
         const attachment = new Discord
-                .MessageAttachment(tempPath, tempFile);
+            .MessageAttachment(tempPath, tempFile);
         
-                const reply = new Discord.MessageEmbed()
+        const reply = new Discord.MessageEmbed()
             .setColor('#008080')
-            .setTitle(`This Weeks Weather Forecast! 🌤`)
+            .setTitle(`parking 🚗`)
             .attachFiles(attachment)
             .setImage(`attachment://${tempFile}`)
             .setDescription(`[View on Web](${lookupURL})`)
             .setThumbnail(url=logo)
             .setTimestamp()
-            page.close();
-    
+        page.close();
         await msg.channel.send(reply)
-        },
+    },
 };
+
+//body > root > app-root
